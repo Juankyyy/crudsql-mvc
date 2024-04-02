a13e7af2b869315a13f9008afc703c15d09cc514
Juanky <juancamiloherrera960@gmail.com>
2024-04-02 12:19:02 -0500

    Vistas Users, Favicon Users

--- crud.sql
+++ crud.sql
@@ -1,8 +1,20 @@
 DROP TABLE Users;
+TRUNCATE TABLE Users;
 
 CREATE TABLE Users (
-	id INT PRIMARY KEY NOT NULL,
+	Id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     Names VARCHAR(45) NOT NULL,
     LastName VARCHAR(45) NOT NULL,
     Email VARCHAR(100) UNIQUE NOT NULL
 );
+
+INSERT INTO Users (Names, LastName, Email)
+VALUES ("Juanky", "Herrera", "juanky@gmail.com"),
+("Mateo", "Velez", "mateo@gmail.com"),
+("Angelica", "Martinez", "angie@gmail.com"),
+("Wilmar", "Puerta", "wilmar@gmail.com"),
+("Isabella", "Guzman", "isa@gmail.com"),
+("Maicol", "Agudelo", "maicol@gmail.com"),
+("Don", "José", "fernando@gmail.com")
+
+SELECT * FROM Users;
